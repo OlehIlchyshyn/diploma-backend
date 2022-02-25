354c29e6bf47da67e08344351514528b5cf48183
Oleh Ilchyshyn <o.ilchyshyn01@gmail.com>
2022-02-25 14:49:12 +0200

    Spring Boot project init

---
+++ src/main/java/com/nulp/fetchproductdata/FetchProductDataApplication.java
@@ -0,0 +1,13 @@
+package com.nulp.fetchproductdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FetchProductDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FetchProductDataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nulp/fetchproductdata/FetchProductDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nulp.fetchproductdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FetchProductDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
