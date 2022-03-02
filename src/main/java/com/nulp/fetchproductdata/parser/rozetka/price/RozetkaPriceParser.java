d4d2c81220fbda8f7b4faa7030d9f477216d0014
Oleh Ilchyshyn <o.ilchyshyn01@gmail.com>
2022-03-02 19:03:04 +0200

    parsing product price from Rozetka API

---
+++ src/main/java/com/nulp/fetchproductdata/parser/rozetka/price/RozetkaPriceParser.java
@@ -0,0 +1,22 @@
+package com.nulp.fetchproductdata.parser.rozetka.price;
+
+import com.google.gson.Gson;
+import com.nulp.fetchproductdata.common.WebClient;
+import com.nulp.fetchproductdata.parser.rozetka.price.model.Price;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RozetkaPriceParser {
+
+    private final String apiLink = "https://common-api.rozetka.com.ua/v2/goods/get-price/?id=";
+
+    public Price getPriceById(long productId) {
+        String json = WebClient.getApiResponse(apiLink + productId);
+
+        return translateJsonToPriceModel(json);
+    }
+
+    private Price translateJsonToPriceModel(String json) {
+        return new Gson().fromJson(json, Price.class);
+    }
+}
---
+++ src/main/java/com/nulp/fetchproductdata/parser/rozetka/price/model/Price.java
@@ -0,0 +1,24 @@
+package com.nulp.fetchproductdata.parser.rozetka.price.model;
+
+import com.google.gson.annotations.SerializedName;
+import lombok.Data;
+
+@Data
+public class Price {
+
+    @SerializedName("old_price")
+    private final double oldPrice;
+
+    @SerializedName("old_usd_price")
+    private final double oldUsdPrice;
+
+    private final double price;
+
+    @SerializedName("usd_price")
+    private final double usdPrice;
+
+    private final String status;
+
+    @SerializedName("sell_status")
+    private final String sellStatus;
+}
