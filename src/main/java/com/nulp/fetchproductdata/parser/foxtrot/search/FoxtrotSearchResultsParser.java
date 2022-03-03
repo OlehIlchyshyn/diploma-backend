3dc78cf7891ce58302a2c4bbaf7bc68730e824e0
Oleh Ilchyshyn <o.ilchyshyn01@gmail.com>
2022-03-03 21:21:14 +0200

    parsing search results from foxtrot website

---
+++ src/main/java/com/nulp/fetchproductdata/parser/foxtrot/search/FoxtrotSearchResultsParser.java
@@ -0,0 +1,64 @@
+package com.nulp.fetchproductdata.parser.foxtrot.search;
+
+import com.nulp.fetchproductdata.parser.foxtrot.search.model.SearchResult;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.springframework.stereotype.Component;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.io.IOException;
+
+@Component
+public class FoxtrotSearchResultsParser {
+
+    private final String searchPageUrl = "https://www.foxtrot.com.ua/uk/search";
+    private final String urlPrefix = "https://www.foxtrot.com.ua";
+    public SearchResult searchByProductTitle(String title) {
+
+        String searchQueryUrl = UriComponentsBuilder.fromUriString(searchPageUrl)
+                .queryParam("query", title)
+                .build()
+                .toUriString();
+
+        try {
+            Document document = Jsoup.connect(searchQueryUrl).get();
+
+            if (document.location().contains("search")) {
+                return parseSearchResultFromHtml(document);
+            } else {
+                return parseReturnedPageAsSearchResult(document);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return new SearchResult(false);
+    }
+
+    private SearchResult parseSearchResultFromHtml(Document htmlDocument) {
+        Elements cards = htmlDocument.getElementsByClass("card__title");
+        if (cards.isEmpty()) {
+            return new SearchResult(false);
+        } else {
+            Element match = cards.get(0);
+            SearchResult searchResult = new SearchResult(true);
+            searchResult.setTitle(match.attr("title"));
+            searchResult.setUrl(urlPrefix + match.attr("href"));
+            return searchResult;
+        }
+    }
+
+    private SearchResult parseReturnedPageAsSearchResult(Document htmlDocument) {
+        Element titleEl = htmlDocument.getElementById("product-page-title");
+        if (titleEl == null) {
+            return new SearchResult(false);
+        } else {
+            SearchResult searchResult = new SearchResult(true);
+            searchResult.setTitle(titleEl.text());
+            searchResult.setUrl(htmlDocument.location());
+            return searchResult;
+        }
+    }
+}
---
+++ src/main/java/com/nulp/fetchproductdata/parser/foxtrot/search/model/SearchResult.java
@@ -0,0 +1,14 @@
+package com.nulp.fetchproductdata.parser.foxtrot.search.model;
+
+import lombok.Data;
+
+@Data
+
+public class SearchResult {
+
+    private final boolean searchSuccessful;
+
+    private String title;
+
+    private String url;
+}
