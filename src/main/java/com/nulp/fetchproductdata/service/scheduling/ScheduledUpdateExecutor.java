2b9c3fa17fdc7bd8a75e672a665a624e442c9125
Oleh Ilchyshyn <o.ilchyshyn01@gmail.com>
2022-03-21 14:56:00 +0200

    Update rates every day at noon

--- src/main/java/com/nulp/fetchproductdata/service/scheduling/ScheduledUpdateExecutor.java
+++ src/main/java/com/nulp/fetchproductdata/service/scheduling/ScheduledUpdateExecutor.java
@@ -1,6 +1,8 @@
 package com.nulp.fetchproductdata.service.scheduling;
 
+import com.nulp.fetchproductdata.repository.ConversionRateRepository;
 import com.nulp.fetchproductdata.repository.ProductRepository;
+import com.nulp.fetchproductdata.service.ConversionService;
 import com.nulp.fetchproductdata.service.UpdatePriceService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -16,12 +18,22 @@ public class ScheduledUpdateExecutor {
 
   private final UpdatePriceService updatePriceService;
   private final ProductRepository productRepository;
+  private final ConversionService conversionService;
+  private final ConversionRateRepository conversionRateRepository;
 
-  @Scheduled(cron = "0 0 12 * * ?")
-  public void updateALlPricesAt12PM() {
+  @Scheduled(cron = "0 0 0 * * ?")
+  public void updateALlPricesAtMidnight() {
     log.info("Started updating product prices");
     List<Long> productIds = productRepository.findAllIds();
     productIds.forEach(updatePriceService::updatePrices);
     log.info("Product prices updating was finished");
+  }
+
+  @Scheduled(cron = "0 0 12 * * ?")
+  public void updateAllRatesAtNoon() {
+    log.info("Started updating conversion rates");
+    conversionRateRepository.deleteAll();
+    conversionRateRepository.saveAll(conversionService.getAllRates());
+    log.info("Conversion rates updating was finished");
   }
 }
